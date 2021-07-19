02d1352c3228cce41e39f26edadb30424707380c
konoha <konohayuya1222@gmail.com>
2021-07-19 10:54:56 +0900

    Player class 追加
    こうすればいいんでしょ

---
+++ app/src/main/java/semonster2/Player.java
@@ -0,0 +1,35 @@
+package semonster2;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import org.w3c.dom.NameList;
+
+
+public class Player {
+  String name;
+  ArrayList<Monster> monsterDeck = new ArrayList<>();
+  LinkedList<Integer> randomNumberList; // random 0 to 4
+
+  Player(LinkedList<Integer> randomNumberList, String playerName){
+    this.randomNumberList = randomNumberList;
+    this.name = playerName;
+  }
+
+  public void drawMonster() {
+     
+    for (int i = 0; i < 5; i++) {
+      this.monsterDeck.add(new Monster(this.randomNumberList.pop(), this.randomNumberList.pop()));
+    }
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Deck:" + this.name + "\n");
+    for (Monster m: this.monsterDeck) {
+      sb.append(m);
+    }
+    return sb.toString();    
+  }
+}
