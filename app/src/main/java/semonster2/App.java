0b663e468c55e72cc68bf70e115eac475503f29f
makilibe <bk.y.bk.orange@gmail.com>
2021-07-19 10:04:03 +0900

    日本語出力やって見たけど無理　getGreetingの変更

--- app/src/main/java/semonster2/App.java
+++ app/src/main/java/semonster2/App.java
@@ -5,7 +5,7 @@ package semonster2;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello SEMonster";
     }
 
     public static void main(String[] args) {
